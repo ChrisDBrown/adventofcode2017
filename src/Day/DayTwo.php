294522a8aa673771f22faec47322f7ed9ac796a6
Chris D Brown <owner@chrisdbrown.co.uk>
2017-12-02 23:20:59 +0000

    Day Two solution

--- src/Day/DayTwo.php
+++ src/Day/DayTwo.php
@@ -3,7 +3,49 @@ declare(strict_types=1);
 
 namespace App\Day;
 
-class DayTwo
+class DayTwo implements DayInterface
 {
+    /**
+     * array
+     */
+    private $sets;
 
+    public function __construct(string $input, bool $isFilePath = true)
+    {
+        if ($isFilePath) $input = file_get_contents($input);
+
+        $this->sets = $this->prepareSets($input);
+    }
+
+    public function partOne(): int
+    {
+        return array_reduce($this->sets, function ($total, $set) {
+            return $total + max($set) - min($set);
+        }, 0);
+    }
+
+    public function partTwo(): int
+    {
+        $total = 0;
+
+        foreach ($this->sets as $set) {
+            foreach ($set as $a) {
+                foreach ($set as $b) {
+                    if ($a % $b === 0 && $a !== $b) {
+                        $total += $a / $b;
+                        continue 2;
+                    }
+                }
+            }
+        }
+
+        return $total;
+    }
+
+    private function prepareSets(string $input): array
+    {
+        return array_map(function($set) {
+            return explode("\t", $set);
+        }, explode("\n", $input));
+    }
 }
--- src/Tests/Day/DayTwoTest.php
+++ src/Tests/Day/DayTwoTest.php
@@ -3,7 +3,20 @@ declare(strict_types=1);
 
 namespace App\Tests\Day;
 
-class DayTwoTest
-{
+use App\Day\DayTwo;
+use PHPUnit\Framework\TestCase;
 
+class DayTwoTest extends TestCase
+{
+    public function testPartOne()
+    {
+        $dayTwo = new DayTwo(__DIR__ . '/fixtures/daytwopartone.txt');
+        $this->assertEquals(18, $dayTwo->partOne());
+    }
+
+    public function testPartTwo()
+    {
+        $dayTwo = new DayTwo(__DIR__ . '/fixtures/daytwoparttwo.txt');
+        $this->assertEquals(9, $dayTwo->partTwo());
+    }
 }
